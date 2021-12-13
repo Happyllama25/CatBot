cd8f74393c3618d576c552823895fd63fcd57b86
Happylama25 <fabsese@outlook.com>
2021-12-12 23:45:26 -0500

    Update Fun.py

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,4 +1,4 @@
-import aiohttp, discord
+import aiohttp, discord, asyncio
 from discord.ext import commands
 
 class Fun(commands.Cog):
@@ -28,6 +28,38 @@ class Fun(commands.Cog):
                 embed.set_image(url = picture)
                 await ctx.send(embed=embed)
 
+    @commands.command(name='remind', help='Reminds you of something with time')
+    async def remind(self, ctx, time, task):
+        def convert(time):
+            pos = ['s', 'm', 'h', 'd']
+
+            time_dict = {"s": 1, "m": 60, "h": 3600, "d": 3600*24}
+
+            unit = time[-1]
+
+            if unit not in pos:
+                return -1
+            try:
+                val = int(time[:-1])
+            except:
+                return -2
+ 
+            return val * time_dict[unit]
+
+        converted_time = convert(time)
+
+        if converted_time == -1:
+            await ctx.send("The time is incorrect")
+            return
+        
+        if converted_time == -2:
+            await ctx.send("Time must be an integer (numbers dum dum)")
+            return
+        
+        await ctx.send(f"Started reminder for **{task}** and will last **{time}**.")
+
+        await asyncio.sleep(converted_time)
+        await ctx.send(f"{ctx.author.mention} your reminder for **{task}** has finished!")
 
 def setup(bot):
     bot.add_cog(Fun(bot))
