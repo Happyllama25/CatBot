7bdb9ba9acb2264efb5d4b35af30873ac0f31f57
Happylama25 <fabsese@outlook.com>
2022-02-23 14:32:05 -0500

    Commit

--- cogs/Panel.py
+++ cogs/Panel.py
@@ -9,8 +9,6 @@ API_KEY = os.getenv('API_KEY')
 
 
 
-
-
 class Panel(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
@@ -31,9 +29,10 @@ class Panel(commands.Cog):
             "Accept": "application/json"
         }
 
-        data = requests.request('GET', url, headers=headers).json()
+        data = requests.get(url, headers=headers).json()
         server_names = []
         server_identifiers = []
+        time = round(data.elapsed.microseconds / 1000)
 
         for server in data['data']:
 
@@ -42,12 +41,11 @@ class Panel(commands.Cog):
         for server in data['data']:
 
             server_identifiers.append(server['attributes'] ['identifier'])
-        authorfoot = str(ctx.author) + f" ⚫ response time: " + f"ZEXOOO"
         embed=discord.Embed(title="Available Servers", color=0x00ffff)
         embed.set_thumbnail(url=ctx.author.avatar_url)
         embed.add_field(name="Name", value='\n'.join(server_names), inline=True)
         embed.add_field(name="Identifier", value='\n'.join(server_identifiers), inline=True)
-        embed.set_footer(text=authorfoot)
+        embed.set_footer(text=str(ctx.author) + f" ⚫ response time: " + str(time))
         await ctx.send(embed=embed)
         server_names.clear()
         server_identifiers.clear()
@@ -65,22 +63,33 @@ class Panel(commands.Cog):
                 "Accept": "application/json"
             }
 
-            dataresources = requests.request('GET', url, headers=headers).json()
+            dataresources = requests.get(url, headers=headers).json()
 
             server_status = dataresources['attributes']['current_state']
             
             memory_unmathed = dataresources['attributes']['resources']['memory_bytes']
 
+            cpu_raw = dataresources['attributes']['resources']['cpu_absolute']
+
+            cpu_usage = round(cpu_raw, 2)
+
             memory = round(memory_unmathed / 1000000000, 2)
 
+            timeresource = round(dataresources.elapsed.microseconds / 1000)
+            timename = round(dataname.elapsed.microseconds / 1000)
+            time = timeresource + timename / 2
+            
+            statusresource = dataresources.status_code
+            statusname = dataname.status_code
+            statuscode = "resource: " + str(statusresource) + " name: " + str(statusname)
             await ctx.send(server_status)
             await ctx.send(str(memory) + ' GB')
 
 
-            #Query for name
+            #Query for name, allocation, port
             url_name = f'https://panel.happyllama25.net/api/client/servers/{id}'
 
-            dataname = requests.request('GET', url_name, headers=headers).json()
+            dataname = requests.get(url_name, headers=headers).json()
             
             name = dataname['attributes']['name']
             await ctx.send(name)
@@ -92,11 +101,74 @@ class Panel(commands.Cog):
 
                 port = server['attributes']['port']
 
-            await ctx.send(port)
-            await ctx.send(allocation)
 
-            
+            await ctx.send(server_status)
+            await ctx.send(str(memory) + ' GB')
+            await ctx.send(str(cpu_usage) + "%")
             await ctx.send(allocation + ':' + str(port))
+
+            if server_status == "running":
+                embed=discord.Embed(title="Server Status", color=0x00ffff)
+                embed.add_field(name="Identifier", value=id, inline=True)
+                embed.add_field(name="Status", value=server_status.capitalize(), inline=True)
+                embed.add_field(name="", value="greencircle", inline=True) # Emoji line
+                embed.add_field(name="Memory Usage", value=str(memory) + ' GB', inline=False)
+                embed.add_field(name="CPU Usage", value=str(cpu_usage) + '%', inline=True)
+                embed.add_field(name="IP:PORT", value=allocation + ':' + str(port), inline=True)
+                embed.set_footer(text=str(ctx.author) + f" ⚫ response time: " + str(time))
+                await ctx.send(embed=embed)
+                # ADD REACTIONS TO START STOP AND RESTART
+
+
+
+
+
+            elif server_status == "offline":
+                await ctx.send('offline')
+                embed=discord.Embed(title="Server Status", color=0x00ffff)
+                embed.add_field(name="Identifier", value=id, inline=True)
+                embed.add_field(name="Status", value=server_status.capitalize(), inline=True)
+                embed.add_field(name="", value="redcircle", inline=True) # Emoji line
+                embed.set_footer(text=str(ctx.author) + f" ⚫ response time: " + str(time))
+                await ctx.send(embed=embed)
+                # ADD REACTIONS TO START STOP AND RESTART
+
+
+
+            elif server_status == "starting":
+                await ctx.send('starting')
+                embed=discord.Embed(title="Server Status", color=0x00ffff)
+                embed.add_field(name="Identifier", value=id, inline=True)
+                embed.add_field(name="Status", value=server_status.capitalize(), inline=True)
+                embed.add_field(name="", value="yellowcircle", inline=True) # Emoji line
+                embed.add_field(name="Memory Usage", value=str(memory) + ' GB', inline=False)
+                embed.add_field(name="CPU Usage", value=str(cpu_usage) + '%', inline=True)
+                embed.add_field(name="IP:PORT", value=allocation + ':' + str(port), inline=True)
+                embed.set_footer(text=str(ctx.author) + f" ⚫ response time: " + str(time))
+                await ctx.send(embed=embed)
+                # ADD REACTIONS TO START STOP AND RESTART
+
+
+
+            else:
+                await ctx.send(statuscode)
+                await ctx.send('Time resource request:' + str(timeresource))
+                await ctx.send('Time name request:' + str(timename))
+                embed=discord.Embed(title="Server Status", color=0x00ffff)
+                embed.set_author(name=statuscode)
+                embed.add_field(name="Identifier", value=id, inline=True)
+                embed.add_field(name="Status", value=server_status.capitalize(), inline=True)
+                embed.add_field(name="", value="yellowcircle", inline=True) # Emoji line
+                embed.add_field(name="Memory Usage", value=str(memory) + ' GB', inline=False)
+                embed.add_field(name="CPU Usage", value=str(cpu_usage) + '%', inline=True)
+                embed.add_field(name="IP:PORT", value=allocation + ':' + str(port), inline=True)
+                embed.set_footer(text=str(ctx.author) + f" ⚫ response time: " + str(time))
+                await ctx.send(embed=embed)
+                # ADD REACTIONS TO START STOP AND RESTART
+
+
+
+
 
 
 
