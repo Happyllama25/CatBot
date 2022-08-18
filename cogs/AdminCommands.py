a3a91f10b0b20acbb29a99954047cd3cd0023f88
Happylama25 <fabsese@outlook.com>
2022-08-18 08:52:23 -0400

    admin commands

--- cogs/AdminCommands.py
+++ cogs/AdminCommands.py
@@ -20,40 +20,40 @@ class AdminCommands(commands.Cog):
     @commands.command()
     @commands.has_permissions(manage_channels=True)
     async def lockdown(self, ctx):
-        try:
-            embed=disnake.Embed(title="⚠️ Server lockdown ⚠️", color=0xff0400)
-            embed.add_field(name="Please wait until lockdown is lifted", value='the roomba is observing')
-            for channel in ctx.guild.channels:
-                channel.set_permissions(target=ctx.guild.default_role, send_messages=False)
-            await ctx.send(embed=embed)
-        except Exception as error:
-            await ctx.send(f'Failed: {error}')
+        # try:
+        #     embed=disnake.Embed(title="⚠️ Server lockdown ⚠️", color=0xff0400)
+        #     embed.add_field(name="Please wait until lockdown is lifted", value='the roomba is observing')
+        #     for channel in ctx.guild.channels:
+        #         channel.set_permissions(target=ctx.guild.default_role, send_messages=False)
+        #     await ctx.send(embed=embed)
+        # except Exception as error:
+        #     await ctx.send(f'Failed: {error}')
+        await ctx.send('not yet')
 
 
     @commands.command()
     @commands.has_permissions(manage_channels=True)
-    async def releaselockdown(self, ctx):
-        try:
-            embed=disnake.Embed(title="Channel unlocked", color=0xff0400)
-            embed.add_field(name="Roomba deployed for cleanup", value='brrrmm')
-            for channel in self.guild.channels:
-                channel.set_permissions(self.guild.default_role, overwrite=None)
-            await ctx.send(embed=embed)
-        except Exception as error:
-            await ctx.send(f'Failed: {error}')
-            
+    async def unlockdown(self, ctx):
+        # try:
+        #     embed=disnake.Embed(title="Channel unlocked", color=0xff0400)
+        #     embed.add_field(name="Roomba deployed for cleanup", value='brrrmm')
+        #     for channel in self.guild.channels:
+        #         channel.set_permissions(self.guild.default_role, overwrite=None)
+        #     await ctx.send(embed=embed)
+        # except Exception as error:
+        #     await ctx.send(f'Failed: {error}')
+        await ctx.send('not yet')            
       
 
     #bans a user with a reason 
     @commands.command()
     @commands.has_role(adminrole)
-    async def ban (self, ctx, member:disnake.User=None, reason =None):
+    async def ban (self, ctx, member:disnake.User=None, reason='not having enough skill'):
         if member == None or member == ctx.message.author:
             message = await ctx.channel.send("Specify user")
             await message.delete(delay=3)
+            await ctx.message.delete(delay=3)
             return
-        if reason == None:
-            reason = "not having enough skill"
         message = f"ur banned from {ctx.guild.name} for {reason}\n\nlol fkin noob"
         await member.send(message)
         await ctx.guild.ban(member, reason=reason)
@@ -75,14 +75,12 @@ class AdminCommands(commands.Cog):
         
     @commands.command()
     @commands.has_role(adminrole)
-    async def unban(self, ctx, member:disnake.User=None, reason=None):
+    async def unban(self, ctx, member:disnake.User=None, reason='has enough skill'):
         if member == None:
-            message = await ctx.channel.send("Specify member")
+            message = await ctx.channel.send("Specify user ID")
             await message.delete(delay=3)
             await ctx.message.delete(delay=3)
             return
-        if reason == None:
-            reason = "has enough skill"
         try: 
             await ctx.guild.unban(user=member, reason=reason)
             await ctx.message.send(f'Unbanned {member}')
@@ -93,12 +91,12 @@ class AdminCommands(commands.Cog):
     @commands.has_permissions(manage_messages = True)
     async def clear(ctx, amount=5):
         await ctx.message.delete()
-        if amount >=31:
+        if amount >= 31:
             await ctx.channel.purge(limit = 30)
             return
         else:
             await ctx.channel.purge(limit = amount)
-        #Add limit to prevent too many deletes
+        #Add limit to prevent too many deletes - done
     
 
 def setup(bot):
