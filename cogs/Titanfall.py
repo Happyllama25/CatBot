bcab716c26b56b3121e01b73dbe79b2deb296b0a
Happylama25 <fabsese@outlook.com>
2023-06-27 01:46:50 +0000

    oh look at that the sun is rising!

---
+++ cogs/Titanfall.py
@@ -0,0 +1,67 @@
+import disnake
+import requests
+from disnake.ext import commands
+
+
+class Titanfall(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(name="titanfall", description="List of active Northstar servers")
+    async def command_name(self, ctx):
+        await ctx.response.defer()
+
+        try:
+            url = "https://northstar.tf/client/servers"
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()
+            data = response.json()
+        except requests.exceptions.RequestException as e:
+            await ctx.send(f"An error occurred while fetching server data: {str(e)}")
+            return
+        except ValueError as e:
+            await ctx.send(f"An error occurred while processing server data: {str(e)}")
+            return
+        
+        servers = data
+        server_count = len(data)
+
+        priority_server = None
+        priority_index = None
+        for index, server in enumerate(servers):
+            if "happyllama25" in server["name"].lower():
+                priority_server = server
+                priority_index = index
+
+        message = f"```diff\n+ {server_count} servers were found - displaying first 5 results\n\n"
+
+        if priority_server:
+            servers.pop(priority_index)
+            servers.insert(0, priority_server)
+        else:
+            message += "- Happyllama25 servers not found\n\n"
+
+        # Sort the servers by player count in descending order
+        servers.sort(key=lambda server: server["playerCount"], reverse=True)
+        total_players = 0
+
+        for server in servers[:5]:
+            message += f"{server['name']}\n"
+            if server['playerCount'] == server['maxPlayers']:
+                message += f"- {server['playerCount']} / {server['maxPlayers']} players connected\n"
+            else:
+                message += f"+ {server['playerCount']} / {server['maxPlayers']} players connected\n"
+            message += f"+ Playing {server['playlist']} on {server['map']}\n\n"
+            if server['hasPassword']:
+                message += "- Password protected\n"
+            total_players += server['playerCount']
+        for server in servers:
+            total_players += server['playerCount']
+
+        message += f"\n+Global total Players: {total_players}"
+
+        await ctx.edit_original_response(f"{message}```")
+
+def setup(bot):
+    bot.add_cog(Titanfall(bot))
--- cogs/Ytdownload.py
+++ cogs/Ytdownload.py
@@ -20,9 +20,22 @@ class Ytdownload(commands.Cog):
         self.bot = bot
         self.last_update = 0  # Add a variable to track the time of the last update
 
+    @commands.slash_command(name='search', description='Search for a YouTube video')
+    async def search(self, ctx, *, query: str):
+        ctx.response.defer()
+        ctx.edit_original_response(content=f"congrats, you found a command i have not finished yet, you get a cookie :cookie: tell happyllama25 to get his ass out of bed and finish this command")
+
     @commands.slash_command(name="download", description="Download a YouTube video")
     async def download(self, ctx, url: str):
+        ctx.response.defer()
         self.ctx = ctx  # Store the ctx object as an instance variable
+
+        # def less_than_a_minute(info, *, incomplete):
+        #     """Download only videos longer than a minute (or with unknown duration)"""
+        #     duration = info.get('duration')
+        #     if duration and duration > 60:
+        #         return 'The video is too long'
+
         ydl_opts = {
             'format': 'best',
             'logger': MyLogger(),
@@ -32,24 +45,24 @@ class Ytdownload(commands.Cog):
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=False)
             video_title = info_dict.get('title', None)
-            await ctx.send(f"Downloading {video_title}...")
+            await ctx.edit_original_response(f"Downloading {video_title}...")
             ydl.download([url])
             video_file = 'downloaded_video.mp4'
             if os.path.getsize(video_file) > 25 * 1024 * 1024:  # Convert 25MB to bytes
-                await ctx.edit_original_response(content=f"The video '{video_title}' is too large to send through Discord.")
+                await ctx.edit_original_response(content=f"The video '{video_title}' is too large to send through Discord. :( LEGALIZE NUCLEAR BOMBS >:)")
             else:
-                await ctx.edit_original_response(content=f"Downloaded {video_title}!")
+                await ctx.edit_original_response(content=f"Downloaded {video_title}! Uploading...")
                 with open(video_file, 'rb') as fp:
                     await ctx.send(file=disnake.File(fp, 'new_video.mp4'))
 
-    def my_hook(self, status):
+    async def my_hook(self, status):
         if status.get('status') == 'downloading':
             loop = asyncio.get_event_loop()
             loop.create_task(self.update_message(status['filename'], status['_percent_str'], status['_eta_str']))
 
     async def update_message(self, filename, percent, eta):
         now = time.time()
-        if now - self.last_update < 5 or percent != 100.0:  # If less than 5 seconds have passed since the last update...
+        if now - self.last_update < 1 or percent == 100.0:  # If less than 5 seconds have passed since the last update...
             return  # ...don't send an update.
         self.last_update = now  # Update the time of the last update.
         await self.ctx.edit_original_response(content=f"Downloading {filename}\nProgress: {percent}\nETA: {eta}")
